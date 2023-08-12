c347dda8ed58fa246c3b1fc4cb5aca8270337743
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-08-12 13:24:49 +0700

    Update Block.java

--- Block.java
+++ Block.java
@@ -8,9 +8,20 @@ public class Block
 	private long timeStamp; //number in milliseconds
 	
 	//Block Constructor 
-	public Block(String data, String previousHash){
-	    this.data = data;
-	    this.previousHash = previousHash;
-	    this.timeStamp = new Date().getTime();
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+	}
+	
+	public String calculateHash(){
+	    String calculateHash = Stringutil.applySha256(
+	            previousHash + 
+	            Long.toString(timeStamp) + 
+	            data
+	            
+	        );
+	    return calculateHash;
 	}
 }
